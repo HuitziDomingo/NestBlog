d99f10b4436fb34b862b33eb0b2866f229680730
HuitziDomingo <huitzitzilidomingo@gmail.com>
2023-02-17 14:55:02 -0600

    Agregar usuarios y mostrarlos

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm'
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  private logger = new Logger('UsersService')
+
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) { }
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      let newUser = this.userRepository.create(createUserDto)
+      return await this.userRepository.save(newUser)
+    } catch (error) {
+      this.handleError(error)
+    }
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userRepository.find()
   }
 
   findOne(id: number) {
@@ -22,5 +39,15 @@ export class UsersService {
 
   remove(id: number) {
     return `This action removes a #${id} user`;
+  }
+
+
+  private handleError(error: any) {
+    if (error === '23505')
+      throw new BadRequestException(error.dtail)
+
+    this.logger.error(error)
+
+    throw new InternalServerErrorException('Los elementos duplicados en la base de datos no son permitidos')
   }
 }
